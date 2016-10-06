1bcb687fde187d45546b370407abbca4701e976a
Convery <convery10122@gmail.com>
2016-10-06 22:37:51 +0200

    Added a global header for includes.

---
+++ Source/STDInclude.h
@@ -0,0 +1,12 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: Apache 2.0
+    Started: 2016-10-6
+    Notes:
+        A global header to simplify loading of configs and utils.
+*/
+
+#pragma once
+
+#include "Configuration/All.h"
+#include "Utility/All.h"
