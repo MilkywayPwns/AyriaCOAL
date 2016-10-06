ad69809bd2c656f61f1b4e4e843d6d01ecd5e5da
Convery <convery10122@gmail.com>
2016-10-06 22:36:15 +0200

    Added headers to include all modules in each folder.

---
+++ Source/Configuration/All.h
@@ -0,0 +1,13 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: Apache 2.0
+    Started: 2016-10-6
+    Notes:
+        Includes all configuration files.
+*/
+
+#pragma once
+
+#include "Warnings.h"
+#include "Defines.h"
+#include "Macros.h"
---
+++ Source/Utility/All.h
@@ -0,0 +1,13 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: Apache 2.0
+    Started: 2016-10-6
+    Notes:
+        Includes all utility classes.
+*/
+
+#pragma once
+
+#include "Files/Filesystem.h"
+#include "Strings/Debugstring.h"
+#include "Strings/Variadicstring.h"
