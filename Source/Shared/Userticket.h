d1f02f83390a4562fe751bb41bc68c5cd7869194
Convery <convery10122@gmail.com>
2016-10-08 01:01:34 +0200

    Declared userticket functionallity

---
+++ Source/Shared/Userticket.h
@@ -0,0 +1,28 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: GPL 2.0
+    Started: 2016-10-7
+    Notes:
+        This encrypted ticket verifies the clients identity.
+        It also contains some information for the servers.
+*/
+
+#pragma once
+#include <cstdint>
+#include <string>
+
+struct Ticket_t
+{
+    std::string Username;
+    uint32_t Expiration;
+    uint32_t IPAddress;
+    uint32_t UserID;
+};
+
+// JSON representation of the ticket to allow for modification between versions.
+std::string SerializeUserticket(Ticket_t &Ticket);
+Ticket_t DeserializeUserticket(std::string &Blob);
+
+// Encryption/decryption on base64 strings.
+std::string EncryptUserticket(std::string &Plain);
+std::string DecryptUserticket(std::string &b64);
